da569eef0c22ce970ef73dbd18ec83dd3fbbcde9
立军王 <18055352658@189.cn>
2019-11-15 14:16:52 +0800

    上传基于230的示例工程和插件包 Android&iOS

---
+++ CLShanYanAPICloudPlugin/iOS/CLSDKShanYanSDKModule/target/CLSDKShanYanSDKModule.framework/Headers/CLSDKShanYanSDKModule.h
@@ -0,0 +1,19 @@
+//
+//  CLSDKShanYanSDKModule.h
+//  CLSDKShanYanSDKModule
+//
+//  Created by wanglijun on 2019/11/14.
+//  Copyright © 2019 wanglijun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLSDKShanYanSDKModule.
+FOUNDATION_EXPORT double CLSDKShanYanSDKModuleVersionNumber;
+
+//! Project version string for CLSDKShanYanSDKModule.
+FOUNDATION_EXPORT const unsigned char CLSDKShanYanSDKModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLSDKShanYanSDKModule/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CLShanYan_APICloud
 闪验APICloud插件及示例工程
+
+ShanYanAPICloudDemo:插件示例工程
+CLShanYanAPICloudPlugin：插件包
